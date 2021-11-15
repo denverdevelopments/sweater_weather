ee19b1e9a428026abdd1947e16043e17e150d842
kevn7n <kevn_nguyen@yahoo.com>
2021-11-14 20:26:36 -0700

    Create forecasts controller, mapquest service

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,1 +1,7 @@
+class ForecastsController < ApplicationController
+  def index
+    long_lat = ForecastFacade.get_long_lat(params[:location])
 
+    # @located = Forecast.search(params[:query]) if params[:query]
+  end
+end
--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -1,9 +1,19 @@
 class MapquestService
-
+  # def self.reviews(id)
+  #   request("/3/movie/#{id}/reviews")[:results]
+  # end
+  #
+  # def self.top_forty(page)
+  #   request("/3/discover/movie?page=#{page}")[:results]
+  # end
   #
   # def self.search_title(query)
   #   request("/3/search/movie?query=#{query}")[:results]
   # end
+
+  def self.find_coordinates(city_state)
+    request("/geocoding/v1/address?key=KEY&location=#{city_state}")[:results]
+  end
 
   private
 
