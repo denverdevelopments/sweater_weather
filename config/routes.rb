bfbacb7837c42d59fb9fb1dec8d8826e9cf3ab4b
kevn7n <kevn_nguyen@yahoo.com>
2021-11-16 20:58:25 -0700

    Create backgrounds controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :forecast, only: [:index]
+      resources :backgrounds, only: [:index]
+      resources :users, only: [:index]
     end
   end
 end
