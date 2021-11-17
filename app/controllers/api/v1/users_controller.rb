32c17317a0f71dc5621fa2425ee428b25a21fde8
kevn7n <kevn_nguyen@yahoo.com>
2021-11-17 07:23:16 -0700

    Fix user create spec, ii passes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,31 +1,18 @@
 class Api::V1::UsersController < ApplicationController
-  # before_action :set_api_key
   before_action :generate_api_key
 
     def create
-      # user = User.new(user_params)
-      # if user.save
-      #   user.update(api_key: @api_key)
-      #   render json: UserSerializer.new(user), status: :created
       details = JSON.parse(request.raw_post, symbolize_names: true)
       details[:api_key] = @api_key
       user = User.new(details)
       if user.save
-          render json: UserSerializer.user(user), status: 201
+          render json: UserSerializer.new(user), status: 201
       else
         render json: { errors: 'Password error' }, status: :bad_request
       end
     end
 
     private
-
-    # def set_api_key
-    #   @api_key = generate_api_key
-    # end
-
-    # def user_params
-    #   params.permit(:email, :password, :password_confirmation)
-    # end
 
     def generate_api_key
       @api_key = SecureRandom.base64.tr('+/=', 'Qrt')
--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -5,54 +5,54 @@ RSpec.describe "Api::V1::Users", type: :request do
     describe 'Happy Path' do
       it 'send a user when succesful creation occurs', :vcr do
 
-        # post '/api/v1/users', params: {
-        #   "email": "test@test.com",
-        #   "password": "password",
-        #   "password_confirmation": "password" }
-        # user = JSON.parse(response.body, symbolize_names: true)
-
-        new_user = {
+        details = {
             "email": "tester@test.com",
             "password": "password",
             "password_confirmation": "password"
         }
 
-        post("/api/v1/users", headers: {"Content_Type": "application/json", "Accept": "application/json"}, params: new_user.to_json)
+        post("/api/v1/users", headers: {"Content_Type": "application/json", "Accept": "application/json"}, params: details.to_json)
         expect(response).to be_successful
 
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        created_user = User.last
-          binding.pry
+        formatted = JSON.parse(response.body, symbolize_names: true)
+        new_user = User.last
 
-        expect(user_response).to be_a(Hash)
-        expect(user_response[:data]).to be_a(Hash)
-        expect(user_response[:data][:id]).to eq(created_user.id)
-        expect(user_response[:data][:type]).to eq("user")
-        expect(user_response[:data][:attributes][:email]).to eq("whatever@example.com")
-        expect(user_response[:data][:attributes][:api_key]).to eq(created_user.api_key)
-
-        # expect(response).to be_successful
-        # expect(response.status).to eq(201)
-        # expect(user[:data][:attributes]).to have_key(:email)
-        # expect(user[:data][:attributes]).to have_key(:api_key)
-        # expect(user[:data][:attributes][:email]).to eq("test@test.com")
-        # expect(User.count).to eq(1)
+        expect(formatted).to be_a(Hash)
+        expect(formatted[:data]).to be_a(Hash)
+        expect(formatted[:data][:id].to_i).to eq(new_user.id)
+        expect(formatted[:data][:type]).to eq("user")
+        expect(formatted[:data][:attributes][:email]).to eq("tester@test.com")
+        expect(formatted[:data][:attributes][:api_key]).to eq(new_user.api_key)
       end
     end
 
     describe 'Sad Path' do
       it 'sends an error when params sent are invalid', :vcr do
 
-        # post '/api/v1/users', params: {
-        #   "email": "tester@test.com",
-        #   "password": "password",
-        #   "password_confirmation": "wrong" }
-        # user = JSON.parse(response.body, symbolize_names: true)
-        #
-        # expect(response.status).to eq(400)
-        # expect(user).to be_a Hash
-        # expect(user).to have_key(:errors)
-        # expect(user[:errors].first).to eq('P')
+        details = {
+            "email": "tester@test.com",
+            "password": "password2",
+            "password_confirmation": "password"
+        }
+        post("/api/v1/users", headers: {"Content_Type": "application/json", "Accept": "application/json"}, params: details.to_json)
+        expect(response).to_not be_successful
+
+        formatted = JSON.parse(response.body, symbolize_names: true)
+        expect(formatted).to have_key(:errors)
+        expect(formatted[:errors]).to eq('Password error')
+      end 
+
+      it 'sends an error when field is empty', :vcr do
+        details = {
+            "password": "password",
+            "password_confirmation": "password"
+        }
+        post("/api/v1/users", headers: {"Content_Type": "application/json", "Accept": "application/json"}, params: details.to_json)
+        expect(response).to_not be_successful
+
+        formatted = JSON.parse(response.body, symbolize_names: true)
+        expect(formatted).to have_key(:errors)
+        expect(formatted[:errors]).to eq('Password error')
       end
     end
   end
