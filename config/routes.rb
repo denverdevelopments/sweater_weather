7a7f482df7c77f5dd2ff8bec6189d64ade5b3f56
kevn7n <kevn_nguyen@yahoo.com>
2021-11-17 09:24:43 -0700

    Update routes, add rt controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  namespace :api do
+namespace :api do
     namespace :v1 do
       resources :forecast, only: [:index]
       resources :backgrounds, only: [:index]
-      resources :users  
-      resources :sessions
+      resources :users, only: [:create]
+      resources :sessions, only: [:create]
+      resources :road_trip, only: [:create]
     end
   end
 
-  #Redirect URI (optional for 'Public' permissions) urn:ietf:wg:oauth:2.0:oob
 end
