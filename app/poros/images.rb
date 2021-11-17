1cd1f3f48a85c37ac5b4c1fea8cd29baaa3ddcd4
kevn7n <kevn_nguyen@yahoo.com>
2021-11-17 01:09:08 -0700

    Finish images spec, poro, it passes

---
+++ app/poros/images.rb
@@ -0,0 +1,21 @@
+class Images
+  attr_reader :id, :type, :top_five
+
+  def initialize(data)
+    @id = nil
+    @type = "image"
+    @top_five = get_details(data)
+  end
+
+  def get_details(input)
+    input.map do |pair|
+      {
+        "creator" => pair[:user][:username],
+        "unsplash_profile" => pair[:user][:links][:portfolio],
+        "location" => pair[:user][:location],
+        "pic_url" => pair[:urls][:full],
+        "source" => "Upsplash.com"
+      }
+    end.first(5)
+  end
+end
